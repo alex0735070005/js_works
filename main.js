b9d92a7bdacf80c8571ae1d78bc6f95cf54c0042
Alexandr Axenov <alex0735070005@gmail.com>
2020-04-11 10:52:23 +0300

    v1

---
+++ main.js
@@ -0,0 +1,27 @@
+var num1 = +prompt("enter num1");
+var num2 = +prompt("enter num2");
+var op = prompt("enter operation */+-");
+
+var res = 0;
+
+if (op === "+") {
+  res = num1 + num2;
+}
+
+if (op === "-") {
+  res = num1 - num2;
+}
+
+if (op === "*") {
+  res = num1 * num2;
+}
+
+if (op === "/") {
+  res = num1 / num2;
+}
+
+if (!res) {
+  alert("You enter only numbers and operation");
+} else {
+  alert(`Your sum: ${res}`);
+}
